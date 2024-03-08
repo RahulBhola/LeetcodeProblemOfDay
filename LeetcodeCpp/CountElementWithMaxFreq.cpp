651dcbbc9f4b912902220d217e2bfd75afc4c9f8
RahulBhola <lovebhola8283@gmail.com>
2024-03-08 21:22:56 +0530

    react native first app

---
+++ LeetcodeCpp/CountElementWithMaxFreq.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <unordered_map>
+#include <climits>
+
+using namespace std;
+
+class Solution
+{
+public:
+    int maxFrequencyElements(vector<int> &nums)
+    {
+        unordered_map<int, int> mp;
+        for (auto &num : nums)
+        {
+            mp[num]++;
+        }
+        int count = 0;
+        int max_Freq = INT_MIN;
+        for (auto &m : mp)
+        {
+            if (m.second > max_Freq)
+                max_Freq = m.second;
+        }
+
+        for (auto &m : mp)
+        {
+            if (m.second == max_Freq)
+                count += max_Freq;
+        }
+        return count;
+    }
+};
