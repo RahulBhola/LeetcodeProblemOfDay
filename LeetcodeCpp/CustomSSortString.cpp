1c64f720b7486f262b3342bba2c3450315b6a455
RahulBhola <lovebhola8283@gmail.com>
2024-03-11 19:58:36 +0530

    Both C++ & Java Coding answers are there

---
+++ LeetcodeCpp/CustomSSortString.cpp
@@ -0,0 +1,25 @@
+#include <iostream> 
+#include <string> 
+#include <unordered_map>
+#include <algorithm>
+using namespace std;
+
+class Solution {
+public:
+    string customSortString(string order, string s) {
+        string result = "";
+        unordered_map<int,int>mp;
+        for(auto &it : s){
+            mp[it]++;
+        }
+        for(auto &it : order){
+            if(mp.find(it) != mp.end())
+                result.append(mp[it], it);
+                mp.erase(it);
+        }
+        for(auto &it : mp){
+            result.append(it.second,it.first);
+        }
+        return result;
+    }
+};
